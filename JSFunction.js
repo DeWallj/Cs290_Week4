c794b0882a48a83121946ca1d64a98da0718ffbc
DeWallj <dewallj@oregonstate.edu>
2016-10-12 20:48:01 +0200

    Added Part 2
    Added comments, name, and completed assignment by adding part 2 code of
    activity 2 homework

--- JSFunction.js
+++ JSFunction.js
@@ -1,5 +1,21 @@
+/*
+*Jennifer DeWall
+*Cs290 Week 3 - Activity 2
+*/
+
+//Call to function that prints out text
 hoistingExample();
 
-function hoistingExample(input){				
-	document.getElementById("output").textContent = "I am a program that calls a function before it is declared.";
+//Function that demonstrates hoisting.
+function hoistingExample(input)
+{				
+	document.getElementById("output1").textContent = "I am a function that has a call to it before it is declared.";
 }
+
+//Call to wontWork() function
+wontWork();
+
+//Function assigned to a variable. A call to it before it is assigned will be shown not to work
+var wontWork = function()
+{   document.getElementById("output2").textContent = "I am a function that is assigned to a variable and is called before it is assigned.";
+};
