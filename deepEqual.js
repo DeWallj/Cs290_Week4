2194fc11c400153449c35848e803b2acec98914c
DeWallj <dewallj@oregonstate.edu>
2016-10-14 19:38:20 +0200

    3rd activity
    Began third activity

---
+++ deepEqual.js
@@ -0,0 +1,16 @@
+/*
+* DeepEqual() 
+* This function takes two arguments and returns true only if they are 
+* the same value or are objects with the same properties whose values are
+* also equal when compared with a recursive call to deepEqual
+*
+* Param @ val1, val2 : two arguments to check
+* Return @ : true if val1 and val2 have same value or are objects
+*    with the same properties
+*/
+function deepEqual(va1, val2)
+{
+	//Check if val1 and val2 are objects and neither null
+	//True, check if val1 and val2 objects have same properties whose vals are equal
+	//Else, check if val1 and val2 have same value
+}
